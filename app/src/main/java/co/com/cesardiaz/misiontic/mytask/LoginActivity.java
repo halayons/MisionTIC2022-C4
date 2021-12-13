364958d0bdced8ac7c334e290c52e486f05725fd
halayons <halayons@unal.edu.co>
2021-12-12 20:31:56 -0500

    PUNTO

---
+++ app/src/main/java/co/com/cesardiaz/misiontic/mytask/LoginActivity.java
@@ -0,0 +1,62 @@
+package co.com.cesardiaz.misiontic.mytask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import co.com.cesardiaz.misiontic.mytask.view.MainActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button signin;
+    DBHelper DB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        username=findViewById(R.id.username);
+        password=findViewById(R.id.password);
+        signin=findViewById(R.id.signin);
+        DB = new DBHelper(this);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+
+                if(TextUtils.isEmpty(user) || TextUtils.isEmpty(pass))
+                    Toast.makeText(LoginActivity.this, "All fields Required", Toast.LENGTH_SHORT).show();
+                else{
+                    Boolean checkuserpass = DB.checkusernamepassword(user,pass);
+                    if(checkuserpass==false){
+                        Boolean insert = DB.insertData(user, pass);
+                        if(insert==true){
+                            Toast.makeText(LoginActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                            intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                        }else{
+                            Toast.makeText(LoginActivity.this,"Wrong password", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(LoginActivity.this,"Login Successfully", Toast.LENGTH_SHORT).show();
+                        intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                    }
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/co/com/cesardiaz/misiontic/mytask/model/MainInteractor.java
@@ -0,0 +1,33 @@
+package co.com.cesardiaz.misiontic.mytask.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import co.com.cesardiaz.misiontic.mytask.mvp.MainMVP;
+import co.com.cesardiaz.misiontic.mytask.view.dto.TaskItem;
+
+public class MainInteractor implements MainMVP.Model {
+//aqui base de datos
+    private List<TaskItem> tempItems;
+
+    public MainInteractor() {
+        tempItems = new ArrayList<>();
+        tempItems.add(new TaskItem("Do the shopping", "November 20, 2021"));
+    }
+
+    @Override
+    public List<TaskItem> getTasks() {
+        return new ArrayList<>(tempItems);
+    }
+
+    @Override
+    public void saveTask(TaskItem task) {
+        tempItems.add(task);
+    }
+
+    @Override
+    public void updateTask(TaskItem item) {
+
+    }
+}
+
